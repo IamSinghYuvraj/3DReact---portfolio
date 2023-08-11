72b797522c7afa9252a52f7e4d1bab3610507956
IamSinghYuvraj <121106713+IamSinghYuvraj@users.noreply.github.com>
2023-08-11 22:00:07 +0530

    About section done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,11 +5,12 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
 
 
 const ServiceCard = ({ index,title,icon }) => {
   return (
-    <Tilt className="xs:w-[250px] w-full">
+    <Tilt className="xs:w-[250px] w-full"> 
       <motion.div
         variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
@@ -62,4 +63,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default SectionWrapper (About, "about");
