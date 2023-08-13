76eec8d6244eef3a488780aa2110c5aea765d1aa
IamSinghYuvraj <121106713+IamSinghYuvraj@users.noreply.github.com>
2023-08-13 20:46:58 +0530

    works section started

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
 
-const Works = () => {
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+
+const ProjectCard = ({ index, name, description, tags, image, source_code_link }) => {
   return (
-    <div>Works</div>
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+        <div className="relative w-full h-[230px]">
+          <img
+            src={image}
+            alt={name}
+            className="w-full h-full object-cover rounded-2xl "
+          />
+
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div 
+              onClick={() => window.open
+              (source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img 
+                src={github}
+                alt="github"
+                className="w-1/2 h-1/2 object-contain"
+              />
+            {/* when the website is uploaded create another link for direct website access */}
+            </div>
+          </div>
+        </div>
+      </Tilt>
+    </motion.div>
   )
 }
 
-export default Works
+const Works = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} `}>My work</p>
+        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+      </motion.div>
+
+      <div className="w-full-flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          These projects provide clear examples of my skills and experience in
+          action. Each project comes with a brief description, links to where
+          you can see the actual code. This collection highlights my ability to
+          solve difficult problems, work with different technologies, and manage
+          projects well.
+        </motion.p>
+      </div>
+
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard 
+            key = {`project-${index}`}
+            index= {index}
+            {...project}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Works, "");
