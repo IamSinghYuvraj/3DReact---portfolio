a6ef47c4bc60d97bc889cdec954dd3c76c05b643
IamSinghYuvraj <121106713+IamSinghYuvraj@users.noreply.github.com>
2023-08-16 18:19:30 +0530

    change in hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,9 +15,9 @@ const Hero = () => {
       <div>
         <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Yuvraj</span></h1>
         <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          I develop 3D websites and<br
+          I am learning to code🧑‍💻<br
           className="sm:block hidden"/>
-          full stack web applications.
+          and make web applications.
         </p>
       </div>
     </div>
